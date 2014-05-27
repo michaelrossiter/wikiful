4da19c927f890323fd9f6f42e7dca1cc517415fa
Michael <michael.b.s.rossiter@gmail.com>
2014-05-27 15:48:10 -0700

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'CaseVids'" do
+      visit '/'
+      expect(page).to have_content('CaseVids')
+    end
+  
+    it "should have the right title" do
+      visit '/'
+      expect(page).to have_title("CaseVids | Home")
+    end
+  end
+
+
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/contact'
+      expect(page).to have_content('Contact')
+    end
+  end
+
+end
