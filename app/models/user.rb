4b05230b868ff0ed8a619adb6534837fe08e63ce
Michael <michael.b.s.rossiter@gmail.com>
2014-05-28 13:39:29 -0700

    make a basic user model, including secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_many :vids, dependent: :destroy
 
   before_save { self.email = email.downcase }
-  before_create :create_remember_token
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
