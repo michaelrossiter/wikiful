a83bbafd45b57ce7fa48381f35c3a0300094a222
Michael <michael.b.s.rossiter@gmail.com>
2014-05-28 12:15:18 -0700

    users controller and landing page moves

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,46 +2,37 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
-
-    it "should have the content 'CaseVids'" do
-      visit '/'
-      expect(page).to have_content('CaseVids')
+    before { visit root_path }
+    it { should have_content('CaseVids') }
+    it { should have_title('CaseVids') }
+    it { should_not have_title('| Home') }
     end
-  
-    it "should have the base title" do
-      visit '/'
-      expect(page).to have_title("CaseVids")
-    end
-  end
-
-
 
   describe "Help page" do
-
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit '/help'
       expect(page).to have_content('Help')
     end
   end
 
-  describe "About page" do
 
+  describe "About page" do
+    before { visit about_path }
     it "should have the content 'About Us'" do
-      visit '/about'
       expect(page).to have_content('About Us')
     end
   end
 
   describe "Contact page" do
-
+    before { visit contact_path }
     it "should have the content 'Contact'" do
-      visit '/contact'
       expect(page).to have_content('Contact')
     end
   
     it "should have the title 'Contact'" do
-      visit '/contact'
       expect(page).to have_title("CaseVids | Contact")
     end
   end
